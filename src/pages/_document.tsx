9bc5d46098eebc8a298bcf1b3e00c3b61ea24903
Mateus Romão <m.romao.ds@gmail.com>
2021-02-27 20:19:18 -0300

    change title name

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default class Mydocument extends Document {
       <Html>
         <Head>
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <meta property="og:title" content="Move.it"/>
+          <meta property="og:title" content="Move.it(Pokemon edition)"/>
           <meta property="og:description" content="Se mantenha saudável na frente do computador."/>
           <meta property="og:image" content="https://i.pinimg.com/originals/f9/05/d2/f905d2c66b32604817eb3f0f9b4e82c0.jpg"/>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
